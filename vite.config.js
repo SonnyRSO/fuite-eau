1a8084163a8e21231fdcfee906b6e931301a60ff
SonnyRSO <s.russo02@outlook.fr>
2026-07-31 10:36:12 +0200

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// IMPORTANT : remplace "releves-eau" par le nom exact de ton dépôt GitHub
-// si tu déploies sur GitHub Pages (https://<user>.github.io/<repo>/)
-export default defineConfig({
-  plugins: [react()],
-  base: '/fuite-eau/',
-})
